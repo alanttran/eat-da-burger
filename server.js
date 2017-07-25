611d52af09a9865f08cdd52fafcb3360f633deb1
alanttran <mr.alan.tran@gmail.com>
2017-07-25 00:41:40 -0700

    set up db

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Routes
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
