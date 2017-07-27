98737dde7ccb5cf080ae6939f96bb46bc76f284b
alanttran <mr.alan.tran@gmail.com>
2017-07-27 01:00:10 -0700

    able to set up project get and create

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,30 @@
 var connection = require("./connection.js");
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function(tableInput, name, cb) {
+        var queryString = "INSERT INTO " + tableInput + " (burger_name, devoured) ";
+        queryString += "VALUES ('";
+        queryString += name.toString();
+        queryString += "', false);";
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -2,19 +2,29 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
+var port = process.env.PORT || 3000;
+
 var app = express();
 
-// Sets an initial port.
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// BodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Routes
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes); 
+
+app.listen(port, function() {
+  console.log("App listening on PORT: " + port);
 });
